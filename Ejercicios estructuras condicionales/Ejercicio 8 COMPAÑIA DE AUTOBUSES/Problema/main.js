052a369c767b44735b5be47c3446ae77fe70b736
Reina <reina_110209@hotmail.com>
2016-04-27 16:16:03 -0500

    new exercises

--- Ejercicios estructuras condicionales/Ejercicio 8 COMPAÑIA DE AUTOBUSES/Problema/main.js
+++ Ejercicios estructuras condicionales/Ejercicio 8 COMPAÑIA DE AUTOBUSES/Problema/main.js
@@ -1,20 +1,22 @@
 //Escribe aquí tu código
-var cantidad_personas = prompt("Cuantas personas irán en el bus?");
-var tipo_bus = prompt("Elige tu tipo de bus");
+function minimo_personas(cantidad_personas) {
+	if (cantidad_personas < 20) {
+		var cantidad_personas = 20;
+	}
+	return cantidad_personas;
+}
 
-
-function calcularCosto(tipo_bus) {
-	var costo_kilometro;
-	var km = prompt("Cuantos kilometros recorrerán?");
+function costo_bus(tipo_bus, km) {
+	var costo_kilometro = 0;
 	switch(tipo_bus) {
 	    case "A":
-	        costo_kilometro = 2.0 * km;
+	        costo_kilometro = parseFloat(2.0 * km);
 	        break;
 	    case "B":
-	        costo_kilometro = 2.5 * km;
+	        costo_kilometro = parseFloat(2.5 * km);
 	        break;
 	    case "C":
-	        costo_kilometro = 3.0 * km;
+	        costo_kilometro = parseFloat(3.0 * km);
 	        break;
 	    default:
 	    	alert("ELIGE ENTRE A - B - C");
@@ -24,5 +26,24 @@ function calcularCosto(tipo_bus) {
 }
 
 
+function calcularCosto(km, cantidad_personas, tipo_bus) {
+
+    cantidad_personas = minimo_personas(cantidad_personas);
+    var costo_persona = costo_bus(tipo_bus, km);
+    var costo_total = costo_persona*cantidad_personas;
+	return [costo_persona, costo_total];
+		
+}
+// entrada del usuario
+var km = prompt("Cuantos kilometros recorrerán?"); 
+var cantidad_personas = prompt("Cuantas personas irán en el bus?");
+var tipo_bus = prompt("Elige tu tipo de bus");
+
+// logica - proceso 
+
+var total = calcularCosto(km, cantidad_personas, tipo_bus);
+// output
+
+alert("El costo por persona será " + total[0] + ". En total será " + total[1]);
 
 
--- Ejercicios estructuras condicionales/Ejercicio 9 HAMBURGUESAS/Problema/main.js
+++ Ejercicios estructuras condicionales/Ejercicio 9 HAMBURGUESAS/Problema/main.js
@@ -1,1 +1,51 @@
 //Escribe aquí tu código
+
+// entrada del usuario 
+
+    var tarjeta = parseInt(prompt("Hola, bienvenido a El náufrago satisfecho :) ¿Efectivo o tarjeta?"));
+    var tipo_burger = parseInt(prompt("¿Qué hamburguesa deseas: Sencilla, Doble o Triple?"));
+	var cantidad_burger = parseInt(prompt("¿Cuántas deseas?"));
+
+// logica - proceso
+
+    var costo_total = costo_hamburguesa(tarjeta,tipo_burger,cantidad_burger);
+
+    function costo_hamburguesa(tarjeta,tipo_burger,cantidad_burger) {
+    	var precio = precio_neto(tipo_burger,cantidad_burger);
+    	var descuento = validar_descuento(tarjeta,precio);
+    	return descuento;
+    }
+    
+    function precio_neto(tipo_burger,cantidad_burger) {
+    	var precio = 0;
+    	switch(tipo_burger) {
+		    case 1:
+		        precio = parseFloat(20.00 * cantidad_burger);
+		        break;
+		    case 2:
+		        precio = parseFloat(25.00 * cantidad_burger);
+		        break;
+		    case 3:
+		        precio = parseFloat(28.00 * cantidad_burger);
+		        break;
+		    default:
+		    	alert("Haz tu pedido de nuevo.");
+		    	break;
+		}
+		return precio;
+    }
+
+    function validar_descuento(tarjeta,precio_neto) {
+    	if(tarjeta === 1) {
+    		return precio_neto * 0.95;
+    	} else {
+    		return precio_neto;
+    	}
+    }
+// output
+
+alert("Tu pedido es " + costo_total);
+
+
+
+	
