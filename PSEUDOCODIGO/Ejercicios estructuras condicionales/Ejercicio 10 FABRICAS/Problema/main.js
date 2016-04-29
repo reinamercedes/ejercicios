6ac23fee1baebf72993e8bb184a8540d5a0b4320
Reina <reina_110209@hotmail.com>
2016-04-29 15:05:43 -0500

    AVANCE

---
+++ PSEUDOCODIGO/Ejercicios estructuras condicionales/Ejercicio 10 FABRICAS/Problema/main.js
@@ -0,0 +1,13 @@
+//Escribe aquí tu código
+
+//entrada del usuario
+
+var materia_prima = prompt("Ingresa la materia prima de los productos");
+
+//logica - proceso
+
+var costo_produccion = materia_prima + mano_obra + gasto_fabric;
+
+var precio_venta = costo_produccion + (costo_produccion * 0.55);
+
+//output
