5e2e682792d838166916b11c8cc3835101fcdecb
Reina <reina_110209@hotmail.com>
2016-05-04 17:15:21 -0500

    avance yee

--- PSEUDOCODIGO/Ejercicios Practicos/Ejercicio 1- Promedio de personas/Problema/main.js
+++ PSEUDOCODIGO/Ejercicios Practicos/Ejercicio 1- Promedio de personas/Problema/main.js
@@ -1,1 +1,20 @@
 //Escribe aquí tú código
+
+var cantidad_personas = prompt("Ingresa cuantas personas son:");
+var suma = 0;
+
+for(var i = 0; i < cantidad_personas; i++){
+	var condicion = true;
+	var estatura;
+	do{
+     		estatura = parseFloat(prompt("Ingrese su estatura:"));
+		if(!isNaN(estatura)){
+			condicion = false;
+		}
+	} while(condicion);
+	suma += estatura;
+}
+
+document.write("El promedio es: " + suma/cantidad_personas);
+
+
--- PSEUDOCODIGO/Ejercicios Practicos/Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
+++ PSEUDOCODIGO/Ejercicios Practicos/Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
@@ -1,22 +1,2 @@
 //Escribe aquí tú código
 
-// entrada del usuario
-var cantidad_sucursales = parseInt(prompt("Ingresa el numero de sucursales"));
-
-// num de sucursales
-// num de tiendas
-// num empleados
-
-// logica - proceso
-
-// calcular el total de ventas:
-// -por tienda
-// sucursales
-// total
-
-// output
-
-// total por tienda
-// por empleado
-// sucursales
-// total total
